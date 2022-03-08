09ed5a714dcea2c4a1fb1b30c3943616ec4ecf9b
michailkuzhlev <michailkuzhlev@yandex.ru>
2022-03-08 13:37:13 +0700

    Урок 9 создал новый package + Фрагмент  LessonNinthFragment

---
+++ app/src/main/java/Lesson9/LessonNinthFragment.java
@@ -0,0 +1,24 @@
+package Lesson9;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.michailkuzhlev.lesson_6.NoteFragment;
+import com.michailkuzhlev.lesson_6.Notes;
+import com.michailkuzhlev.lesson_6.R;
+
+public class LessonNinthFragment extends Fragment {
+
+    public static LessonNinthFragment newInstance() {
+        LessonNinthFragment fragment = new LessonNinthFragment();
+        return fragment;
+    }
+
+
+}
